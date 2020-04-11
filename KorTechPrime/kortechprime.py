b61004e527cd451650fdc353a20d0a2766c8dbd1
FrozenNinja <golden_triforce@hotmail.com>
2020-04-11 12:55:15 -0700

    Update kortechprime.py

--- KorTechPrime/kortechprime.py
+++ KorTechPrime/kortechprime.py
@@ -13,14 +13,15 @@ class KorTechPrime(commands.Cog):
                 guild = ctx.message.guild
                 author = ctx.message.author
 
-                role_upd = 354105758159339541
+                role_memb = discord.utils.get(ctx.guild.roles, name="TITO Member")
+                role_upd = discord.utils.get(ctx.guild.roles, name="Updating")
 
-                if (author.Role != 323631110997147659):
+                if role_memb not in ctx.author.roles:
                         await ctx.send("You are not masked as a TITO Member")
                         return
 
                 try:
-                        await self.bot.add_roles(author, role_upd)
+                        await ctx.author.add_roles(role_upd)
                 except discord.errors.Forbidden as err:
                         await ctx.send("I don't have permissions to mark you, {}: {}.".format(author.mention, err))
                 except AttributeError:  # role_to_add is NoneType
