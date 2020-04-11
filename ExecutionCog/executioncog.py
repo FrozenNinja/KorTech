2f4c76357c1a01150d38b8571ae4c0eda737a81e
FrozenNinja <62780360+FrozenNinja@users.noreply.github.com>
2020-04-11 10:59:30 -0700

    Update executioncog.py

--- ExecutionCog/executioncog.py
+++ ExecutionCog/executioncog.py
@@ -9,34 +9,34 @@ class ExecutionCog(commands.Cog):
 
         author = ctx.message.author	
         #Text
-        if author.id == "277453509018779648":
-            if user.id == "116329127732051974":
+        if author.id == 277453509018779648:
+            if user.id == 116329127732051974:
                 await ctx.send("*" + user.mention + " dodges the bullet like a ninja" + "*")
             else:
                 await ctx.send("*" + author.mention + " executes " + user.mention + " with Nagant M1895 Revolver" + "*")
-        elif author.id == "116329127732051974":
-            if user.id == "177403864369070080":
+        elif author.id == 116329127732051974:
+            if user.id == 177403864369070080:
                 await ctx.send("*" + author.mention + " summons **MechaKorTech**!\n" + user.mention + " summons\n**MechaAschDroid**!\nThey start to battle and destroy everything around them!" + "*")
             else:
                 await ctx.send("*" + author.mention + " summons **MechaKorTech** to blast " + user.mention + " into smithereens!" + "*")
-        elif author.id == "177403864369070080":
-            if user.id == "116329127732051974":
+        elif author.id == 177403864369070080:
+            if user.id == 116329127732051974:
                 await ctx.send("*" + user.mention + " and " + author.mention + " REEEEE at eachother until everyone else's heads explode" + "*")
             else:
                 await ctx.send("*" + author.mention + " REEEEEs at" + user.mention + "until their head explodes" + "*")
-        elif author.id == "313367342966898689":
+        elif author.id == 313367342966898689:
             await ctx.send("*" + author.mention + " hugs " + user.mention + " and smothers them to death" + "*")
-        elif author.id == "129537561101074433":
+        elif author.id == 129537561101074433:
             await ctx.send("*" + author.mention + " reads The Wealth of Nations to " + user.mention + " until they die from boredom" + "*")
-        elif author.id == "214367240991014912":
+        elif author.id == 214367240991014912:
             await ctx.send("*" + author.mention + " throws a blue shell, sending " + user.mention + " straight to last place!" + "*")
-        elif author.id == "277232149957050378":
+        elif author.id == 277232149957050378:
             await ctx.send("*" + author.mention + " has a haiku for " + user.mention + "*" + "```My blade slices forth\nExecution comes at last\nDo not fail next time```")
-        elif author.id == "304481265233559554":
+        elif author.id == 304481265233559554:
             await ctx.send("*" + user.mention + " has failed our Glorious Leader " + author.mention + " and has thus forfeit their life." + "*")
-        elif author.id == "286367887168634882":
+        elif author.id == 286367887168634882:
             await ctx.send("*" + author.mention + " needs to let Kort know what she wants as a command" + "*")
-        elif author.id == "200525863060635649":
+        elif author.id == 200525863060635649:
             await ctx.send("*" + author.mention + " needs to let Kort know what he wants as a command" + "*")
         else:
             await ctx.send("You aren't Häme!")
