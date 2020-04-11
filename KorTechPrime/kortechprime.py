42db8fe57b7bdd348929069bcb3005c07eee868f
FrozenNinja <golden_triforce@hotmail.com>
2020-04-11 16:39:42 -0700

    Update kortechprime.py

--- KorTechPrime/kortechprime.py
+++ KorTechPrime/kortechprime.py
@@ -36,3 +36,31 @@ class KorTechPrime(commands.Cog):
                 await ctx.author.remove_roles(role_upd)
                 emoji = '<:oof:549276828158918656>'
                 await ctx.message.add_reaction(emoji)
+
+    @commands.command()
+    async def update_over(self, ctx: commands.Context):
+                """Mark that update is finished."""
+
+                guild = ctx.message.guild
+
+                role_cmd = discord.utils.get(ctx.guild.roles, name="KPCmd")
+                role_memb = discord.utils.get(ctx.guild.roles, name="TITO Member")
+                role_ally = discord.utils.get(ctx.guild.roles, name="Allied Military")
+                role_upd = discord.utils.get(ctx.guild.roles, name="Updating")
+				
+                if not role_cmd in ctx.author.roles:
+                        await ctx.send("Not authorized to use this command.")
+                        return
+
+                for member in guild.members:
+                        if not role_upd in member.roles:
+                                continue
+
+                        try:
+                                await ctx.member.remove_roles(role_upd)
+                        except discord.errors.Forbidden:
+                                await self.bot.say("%s: Failed to unmask - I don't have permissions to do that." % ctx.member.name)
+                        except AttributeError:  # role_to_add is NoneType
+                                await self.bot.say("%s: Failed to unmask - That role isn't user settable." % ctx.member.name)
+                        else:
+                                await self.bot.say("%s: Unmasked." % ctx.member.mention)
