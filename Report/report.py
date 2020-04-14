5a7f18c3e1bb710ae6390e37c7b848b028778378
FrozenNinja <golden_triforce@hotmail.com>
2020-04-13 21:19:06 -0700

    Update report.py

--- Report/report.py
+++ Report/report.py
@@ -191,17 +191,17 @@ Endorsements Received: {} -- {}
 
     async def _ne(self, ctx, *, wanation: str):
         """Nations Endorsing the specified WA nation"""
-        origne = (self._endocheck(await Api(
-            "endorsements wa", nation=wanation))["endorsements"].replace(",", self.delim))
+        root = await Api("endorsements fullname wa", nation=wa_nation)
+        if root.UNSTATUS.pyval.lower() == "non-member":
+            return False
+        origne = (await Api("endorsements wa", nation=wanation)["endorsements"].replace(",", self.delim))
         ne = origne.replace("_", " ")
         return ne
 
     async def _nec(self, ctx, *, wanation: str):
         """Number of Nations Endorsing (Count) the specified WA nation"""
-        nec = (self._endocheck(await Api("census wa", nation=wanation, scale="66", mode="score"))["censusscore"]["text"])
+        root = await Api("endorsements fullname wa", nation=wa_nation)
+        if root.UNSTATUS.pyval.lower() == "non-member":
+            return False
+        nec = await Api("census wa", nation=wanation, scale="66", mode="score")["censusscore"]["text"])
         return nec
-
-    def _endocheck(self, ctx, data):
-        if data["unstatus"] == "Non-member":
-            raise commands.BadArgument("Nation {} is not in the WA.".format(data["id"]))
-        return data
