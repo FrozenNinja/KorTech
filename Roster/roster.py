91185842f69606e4a1fff01d000a6fcf71f26836
FrozenNinja <golden_triforce@hotmail.com>
2020-09-14 23:16:49 -0700

    Update roster.py

--- Roster/roster.py
+++ Roster/roster.py
@@ -30,7 +30,7 @@ class Roster(commands.Cog):
         self.nsapi = self.bot.get_cog('NSApi')
         
         #Checks that previous nation is no longer WA
-        oldnation = await self.config.user(ctx.user).userwa()
+        oldnation = await self.config.user(user).userwa()
         if oldnation != "Null":
             wa = await self._isinwa(wanation=oldnation)
             if "non-member" in wa.lower():
@@ -38,7 +38,7 @@ class Roster(commands.Cog):
                 newwa = await self._isinwa(wanation=newnation)
                 if "non-member" not in newwa.lower():
                     #Saves new WA in Roster
-                    await self.config.user(ctx.user).userwa.set(newnation)
+                    await self.config.user(user).userwa.set(newnation)
                 else:
                     await ctx.send("Make sure Nation given is in the WA")
             else:
@@ -48,7 +48,7 @@ class Roster(commands.Cog):
             newwa = await self._isinwa(wanation=newnation)
             if "non-member" not in newwa.lower():
                 #Saves new WA in Roster
-                await self.config.user(ctx.user).userwa.set(newnation)    
+                await self.config.user(user).userwa.set(newnation)    
             else:
                 await ctx.send("Make sure Nation given is in the WA")
 
@@ -57,7 +57,7 @@ class Roster(commands.Cog):
         user = ctx.message.author
         
         #Lists current WA nation for self
-        currentwa = await self.config.user(ctx.user).userwa()
+        currentwa = await self.config.user(user).userwa()
         await ctx.send(currentwa)
     
     #async def roster(self, ctx):
