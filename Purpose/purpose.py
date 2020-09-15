0043bb191f4924bba328f95f04be5c0bd1403289
FrozenNinja <golden_triforce@hotmail.com>
2020-09-15 13:51:22 -0700

    Update purpose.py

--- Purpose/purpose.py
+++ Purpose/purpose.py
@@ -10,15 +10,15 @@ class Purpose(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         
-    messages = 0
+    self.messages = 0
 
     @commands.Cog.listener()
     async def on_command(self, ctx):
 
         n = random.randint(2,5)
         
-        if messages >= n:
+        if self.messages >= n:
             await ctx.send("test")
-            global messages = 0
+            self.messages = 0
         else:
-            global messages +=1
+            self.messages +=1
