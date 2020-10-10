a40abc976101493929a0f9df5f57d98164f2cde8
FrozenNinja <golden_triforce@hotmail.com>
2020-10-10 16:45:32 -0700

    Update roster.py

--- Roster/roster.py
+++ Roster/roster.py
@@ -82,7 +82,7 @@ class Roster(commands.Cog):
         tostring = json.dumps(rosterdict, sort_keys=True, indent=0)
 
         nav = pag.EmbedNavigatorFactory(max_lines=12, prefix="__**TITO Roster**__", enable_truncation=True)
-        nav += tostring.strip('{').replace('":',"\n").replace('",',"").replace('"',"**").strip('*}')
+        nav += tostring.strip('{}').replace('":',"\n").replace('",',"").replace('"',"**").rstrip('\n').rstrip('*')
 
         nav.start(ctx)
 
