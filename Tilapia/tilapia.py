b8d13af01897800c3e1f414cf71a1de007f7f6c6
FrozenNinja <golden_triforce@hotmail.com>
2021-07-06 00:40:58 -0400

    Update tilapia.py

--- Tilapia/tilapia.py
+++ Tilapia/tilapia.py
@@ -5,16 +5,12 @@ from redbot.core import commands
 class Tilapia(commands.Cog):
     """Tilapia"""
 
-    def is_channel():
-        def predicate(ctx):
-            return ctx.message.channel.id == 748238663862845502
-        return commands.check(predicate)
-
     def __init__(self, bot):
         self.bot = bot
 
     @commands.Cog.listener()
     @is_channel()
     async def on_message_without_command(self, message):
-        if ":tilapia:" not in message.content:
-            await message.delete()
+        if ctx.message.channel.id == 748238663862845502
+            if ":tilapia:" not in message.content:
+                await message.delete()
