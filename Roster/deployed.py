bf2e704f71c1dc92b68ae9301a0fe42747487ad2
HN67 <hn67allardkids@gmail.com>
2021-08-19 23:53:30 -0600

    Add module to check deployed nations

---
+++ Roster/deployed.py
@@ -0,0 +1,50 @@
+"""Use a roster file to find who is deployed in regions.
+
+Based on https://github.com/HN67/nsapi/blob/master/roster/deployed.py
+but adapted to work with sans."""
+
+import typing as t
+
+import sans
+from sans.api import Api
+from sans.utils import pretty_string
+
+
+def clean_format(nation: str) -> str:
+    """Converts a nation name to API form."""
+    return nation.replace(" ", "_").lower()
+
+
+async def endorsements(nation: str) -> t.Iterable[str]:
+    """Query NS API for the endorsements of a nation."""
+    Api.agent = "10000 Islands Discord Bot contact Kortexia"
+    request = Api(
+        "endorsements",
+        nation=nation,
+    )
+    try:
+        root = await request
+    except sans.errors.HTTPException:
+        return []
+    else:
+        pretty = pretty_string(root)
+        return pretty.split(",")
+
+
+async def deployed(
+    lead: str,
+    roster: t.Mapping[str, t.Iterable[str]],
+) -> t.Collection[str]:
+    """Determine who are deployed and endorsing the lead."""
+    # Obtain endorsement list of lead
+    endos = map(clean_format, await endorsements(lead))
+    # Invert roster into puppet -> main form
+    owners = {
+        clean_format(switcher): main
+        for main, switchers in roster.items()
+        for switcher in switchers
+    }
+    # Also include main nations
+    owners.update({main: main for main in roster.keys()})
+    # Return owners who have a nation endoing lead
+    return [owners[nation] for nation in endos if nation in owners]
