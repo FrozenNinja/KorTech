52567c37633dc296e42a6525ad441f27ce20c9d8
HN67 <hn67allardkids@gmail.com>
2021-11-26 09:59:58 -0700

    Autoformat with black

--- KorTechPrime/kortechprime.py
+++ KorTechPrime/kortechprime.py
@@ -12,129 +12,139 @@ class KorTechPrime(commands.Cog):
 
     @commands.command()
     async def isupdate(self, ctx: commands.Context, arg):
-                """Designate whether its UpdateTime or not, please use Yes or No"""
+        """Designate whether its UpdateTime or not, please use Yes or No"""
 
-                guild = ctx.message.guild
+        guild = ctx.message.guild
 
-                role_cmd = discord.utils.get(ctx.guild.roles, name="KPCmd")
-				
-                if not role_cmd in ctx.author.roles:
-                        await ctx.send("You are not authorized to use this command.")
-                        return
-						
-                if arg.lower() == "yes":
-                   self.updatetime = True
-                   await ctx.send("Update is running!")
-                elif arg.lower() == "no":
-                   self.updatetime = False
-                   await ctx.send("It's no longer time for Update")
-                else:
-                    await ctx.send("Please answer Yes or No")
+        role_cmd = discord.utils.get(ctx.guild.roles, name="KPCmd")
+
+        if not role_cmd in ctx.author.roles:
+            await ctx.send("You are not authorized to use this command.")
+            return
+
+        if arg.lower() == "yes":
+            self.updatetime = True
+            await ctx.send("Update is running!")
+        elif arg.lower() == "no":
+            self.updatetime = False
+            await ctx.send("It's no longer time for Update")
+        else:
+            await ctx.send("Please answer Yes or No")
 
     @commands.command()
     async def imhere(self, ctx: commands.Context):
-                """Mark that the user is present for update."""
+        """Mark that the user is present for update."""
 
-                guild = ctx.message.guild
-                author = ctx.message.author
+        guild = ctx.message.guild
+        author = ctx.message.author
 
-                role_memb = discord.utils.get(ctx.guild.roles, name="TITO Member")
-                role_ally = discord.utils.get(ctx.guild.roles, name="Allied Military")
-                role_upd = discord.utils.get(ctx.guild.roles, name="Updating")
+        role_memb = discord.utils.get(ctx.guild.roles, name="TITO Member")
+        role_ally = discord.utils.get(ctx.guild.roles, name="Allied Military")
+        role_upd = discord.utils.get(ctx.guild.roles, name="Updating")
 
-                if self.updatetime == True:
-                    if (role_memb in ctx.author.roles) or (role_ally in ctx.author.roles):
-                        await ctx.author.add_roles(role_upd)
-                        emoji = '<:tito:351110740259897349>'
-                        await ctx.message.add_reaction(emoji)
-                    else:
-                        await ctx.send("You are not masked as TITO Member or Allied Military")
-                else:
-                    await ctx.send("Try again when its time for Update!")
+        if self.updatetime == True:
+            if (role_memb in ctx.author.roles) or (role_ally in ctx.author.roles):
+                await ctx.author.add_roles(role_upd)
+                emoji = "<:tito:351110740259897349>"
+                await ctx.message.add_reaction(emoji)
+            else:
+                await ctx.send("You are not masked as TITO Member or Allied Military")
+        else:
+            await ctx.send("Try again when its time for Update!")
 
     @commands.command()
     async def nohere(self, ctx: commands.Context):
-                """Mark that the user is not present for update."""
+        """Mark that the user is not present for update."""
 
-                guild = ctx.message.guild
-                author = ctx.message.author
+        guild = ctx.message.guild
+        author = ctx.message.author
 
-                role_upd = discord.utils.get(ctx.guild.roles, name="Updating")
+        role_upd = discord.utils.get(ctx.guild.roles, name="Updating")
 
-                await ctx.author.remove_roles(role_upd)
-                emoji = '<:oof:549276828158918656>'
-                await ctx.message.add_reaction(emoji)
+        await ctx.author.remove_roles(role_upd)
+        emoji = "<:oof:549276828158918656>"
+        await ctx.message.add_reaction(emoji)
 
     @commands.command()
     async def rampage(self, ctx: commands.Context):
-                """Mark that update is finished."""
+        """Mark that update is finished."""
 
-                guild = ctx.message.guild
+        guild = ctx.message.guild
 
-                role_cmd = discord.utils.get(ctx.guild.roles, name="KPCmd")
-                role_upd = discord.utils.get(ctx.guild.roles, name="Updating")
-				
-                if not role_cmd in ctx.author.roles:
-                        await ctx.send("You are not authorized to use this command.")
-                        return
+        role_cmd = discord.utils.get(ctx.guild.roles, name="KPCmd")
+        role_upd = discord.utils.get(ctx.guild.roles, name="Updating")
 
-                for Member in guild.members:
-                        if not role_upd in Member.roles:
-                                continue
+        if not role_cmd in ctx.author.roles:
+            await ctx.send("You are not authorized to use this command.")
+            return
 
-                        try:
-                                await Member.remove_roles(role_upd)
-                        except discord.errors.Forbidden:
-                                await ctx.send("Failed to unmask - I don't have permissions to do that.")
-                        else:
-                                await ctx.send("%s: Unmasked." % Member.mention)
+        for Member in guild.members:
+            if not role_upd in Member.roles:
+                continue
+
+            try:
+                await Member.remove_roles(role_upd)
+            except discord.errors.Forbidden:
+                await ctx.send(
+                    "Failed to unmask - I don't have permissions to do that."
+                )
+            else:
+                await ctx.send("%s: Unmasked." % Member.mention)
 
     @commands.command()
     async def radio_silence(self, ctx: commands.Context):
-                """Silence peeps."""
+        """Silence peeps."""
 
-                guild = ctx.message.guild
+        guild = ctx.message.guild
 
-                role_cmd = discord.utils.get(ctx.guild.roles, name="KPCmd")
-                role_mute = discord.utils.get(ctx.guild.roles, name="Muted")
-                role_upd = discord.utils.get(ctx.guild.roles, name="Updating")
-				
-                if not role_cmd in ctx.author.roles:
-                        await ctx.send("You are not authorized to use this command.")
-                        return
-                else:
-                    await ctx.send("Radio Silence has been enacted, pay attention to posted orders")
+        role_cmd = discord.utils.get(ctx.guild.roles, name="KPCmd")
+        role_mute = discord.utils.get(ctx.guild.roles, name="Muted")
+        role_upd = discord.utils.get(ctx.guild.roles, name="Updating")
 
-                for Member in guild.members:
-                        if not role_upd in Member.roles:
-                                continue
+        if not role_cmd in ctx.author.roles:
+            await ctx.send("You are not authorized to use this command.")
+            return
+        else:
+            await ctx.send(
+                "Radio Silence has been enacted, pay attention to posted orders"
+            )
 
-                        try:
-                                await Member.add_roles(role_mute)
-                        except discord.errors.Forbidden:
-                                await ctx.send("Failed to unmask - I don't have permissions to do that.")
+        for Member in guild.members:
+            if not role_upd in Member.roles:
+                continue
+
+            try:
+                await Member.add_roles(role_mute)
+            except discord.errors.Forbidden:
+                await ctx.send(
+                    "Failed to unmask - I don't have permissions to do that."
+                )
 
     @commands.command()
     async def silence_over(self, ctx: commands.Context):
-                """Un-Silence peeps."""
+        """Un-Silence peeps."""
 
-                guild = ctx.message.guild
+        guild = ctx.message.guild
 
-                role_cmd = discord.utils.get(ctx.guild.roles, name="KPCmd")
-                role_mute = discord.utils.get(ctx.guild.roles, name="Muted")
-                role_upd = discord.utils.get(ctx.guild.roles, name="Updating")
-				
-                if not role_cmd in ctx.author.roles:
-                        await ctx.send("You are not authorized to use this command.")
-                        return
-                else:
-                    await ctx.send("Radio Silence has been disabled, resume normal conversation")
+        role_cmd = discord.utils.get(ctx.guild.roles, name="KPCmd")
+        role_mute = discord.utils.get(ctx.guild.roles, name="Muted")
+        role_upd = discord.utils.get(ctx.guild.roles, name="Updating")
 
-                for Member in guild.members:
-                        if not role_mute in Member.roles:
-                                continue
+        if not role_cmd in ctx.author.roles:
+            await ctx.send("You are not authorized to use this command.")
+            return
+        else:
+            await ctx.send(
+                "Radio Silence has been disabled, resume normal conversation"
+            )
 
-                        try:
-                                await Member.remove_roles(role_mute)
-                        except discord.errors.Forbidden:
-                                await ctx.send("Failed to unmask - I don't have permissions to do that.")
+        for Member in guild.members:
+            if not role_mute in Member.roles:
+                continue
+
+            try:
+                await Member.remove_roles(role_mute)
+            except discord.errors.Forbidden:
+                await ctx.send(
+                    "Failed to unmask - I don't have permissions to do that."
+                )
