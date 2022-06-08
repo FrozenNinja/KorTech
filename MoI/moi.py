53bf509bc1327fba113b7bbedf7d191c3b0c2a39
FrozenNinja <golden_triforce@hotmail.com>
2022-06-08 19:02:01 -0400

    Update moi.py

--- MoI/moi.py
+++ MoI/moi.py
@@ -7,13 +7,6 @@ from redbot.core import commands
 class MoI(commands.Cog):
     """10KI Cog for Minister of Immigration role management"""
 
-    role_monthlytop = discord.utils.get(ctx.guild.roles, name="Recruiter of the Month")
-    role_weeklytop = discord.utils.get(ctx.guild.roles, name="Recruiter of the Week")
-    role_2000tg = discord.utils.get(ctx.guild.roles, name="2000 Telegrams Sent Monthly!")
-    role_1000tg = discord.utils.get(ctx.guild.roles, name="1000 Telegrams Sent Monthly!")
-    role_500tg = discord.utils.get(ctx.guild.roles, name="500 Telegrams Sent Monthly!")
-    role_250tg = discord.utils.get(ctx.guild.roles, name="250 Telegrams Sent Monthly!")
-
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
@@ -29,6 +22,7 @@ class MoI(commands.Cog):
         ) -> None:
         """Gives Recruiter of the Month role and takes it from previous"""
         guild = ctx.message.guild
+        role_monthlytop = discord.utils.get(ctx.guild.roles, name="Recruiter of the Month")
 
         for member in guild.members:
             try:
@@ -46,6 +40,7 @@ class MoI(commands.Cog):
     ) -> None:
         """Gives Recruiter of the Week role and takes it from previous"""
         guild = ctx.message.guild
+        role_weeklytop = discord.utils.get(ctx.guild.roles, name="Recruiter of the Week")
 
         for member in guild.members:
             try:
@@ -62,6 +57,8 @@ class MoI(commands.Cog):
         self, ctx: commands.Context, user: discord.Member = None
     ) -> None:
         """Gives 2000 Telegrams Sent Monthly! role"""
+        role_2000tg = discord.utils.get(ctx.guild.roles, name="2000 Telegrams Sent Monthly!")
+
         try:
             await user.add_roles(role_2000tg)
         except discord.Forbidden:
@@ -72,6 +69,8 @@ class MoI(commands.Cog):
         self, ctx: commands.Context, user: discord.Member = None
     ) -> None:
         """Gives 1000 Telegrams Sent Monthly! role"""
+        role_1000tg = discord.utils.get(ctx.guild.roles, name="1000 Telegrams Sent Monthly!")
+
         try:
             await user.add_roles(role_1000tg)
         except discord.Forbidden:
@@ -82,6 +81,8 @@ class MoI(commands.Cog):
         self, ctx: commands.Context, user: discord.Member = None
     ) -> None:
         """Gives 500 Telegrams Sent Monthly! role"""
+        role_500tg = discord.utils.get(ctx.guild.roles, name="500 Telegrams Sent Monthly!")
+
         try:
             await user.add_roles(role_500tg)
         except discord.Forbidden:
@@ -92,6 +93,8 @@ class MoI(commands.Cog):
         self, ctx: commands.Context, user: discord.Member = None
     ) -> None:
         """Gives 250 Telegrams Sent Monthly! role"""
+        role_250tg = discord.utils.get(ctx.guild.roles, name="250 Telegrams Sent Monthly!")
+
         try:
             await user.add_roles(role_250tg)
         except discord.Forbidden:
@@ -104,6 +107,10 @@ class MoI(commands.Cog):
     ) -> None:
         """Removes all monthly roles from users"""
         guild = ctx.message.guild
+        role_2000tg = discord.utils.get(ctx.guild.roles, name="2000 Telegrams Sent Monthly!")
+        role_1000tg = discord.utils.get(ctx.guild.roles, name="1000 Telegrams Sent Monthly!")
+        role_500tg = discord.utils.get(ctx.guild.roles, name="500 Telegrams Sent Monthly!")
+        role_250tg = discord.utils.get(ctx.guild.roles, name="250 Telegrams Sent Monthly!")
 
         for member in guild.members:
             try:
