f2fd965f6d9e97b2e5645fd0fa1eca8b3c0a98b9
FrozenNinja <golden_triforce@hotmail.com>
2022-12-06 19:50:16 -0500

    Update question.py

--- question/question.py
+++ question/question.py
@@ -8,7 +8,8 @@ class Question(commands.Cog):
     async def question(self, ctx):
         """Counts down from 7"""
         i = 7
-        for(i > 0):
+        while(i > 0):
 		    await ctx.send(i)
             time.sleep(1)
+            i = i - 1
         await ctx.send("Answer Window closed")
