cd589193959bf290ce398af1626b139aac7ecc6b
FrozenNinja <golden_triforce@hotmail.com>
2022-12-06 19:47:13 -0500

    Test Cog for Asche

---
+++ question/question.py
@@ -0,0 +1,14 @@
+from redbot.core import commands
+import time
+
+class Question(commands.Cog):
+    """Description of the cog visible with [p]help MyFirstCog"""
+
+    @commands.command()
+    async def question(self, ctx):
+        """Counts down from 7"""
+        i = 7
+        for(i > 0):
+		    await ctx.send(i)
+            time.sleep(1)
+        await ctx.send("Answer Window closed")
