d0929e79e1bcb28f6070aeaa5f67e16cfef0d5a2
FrozenNinja <golden_triforce@hotmail.com>
2023-02-23 23:17:02 -0500

    Update executioncog.py
    Added new Command, removed old

--- ExecutionCog/executioncog.py
+++ ExecutionCog/executioncog.py
@@ -7,17 +7,19 @@ class ExecutionCog(commands.Cog):
     @commands.command()
     async def execute(self, ctx: commands.Context, user: discord.Member):
 
-        author = ctx.message.author	
+        author = ctx.message.author    
         #Text
         if user.id == 351596701913448449: #KorTech
             await ctx.send("Attempting to kill me with my own power? Foolish mortal.\n" + "*" + "Electrocutes " + author.mention + " until they turn into ash." + "*")
-        elif user.id == 313367342966898689: #Hakke
-            await ctx.send("*" + user.mention + " laughs at " + author.mention + " and proceeds to drain them of all their blood" + "*")
+        elif user.id == 304481265233559554: #MarkProtect
+            await ctx.send("*" + user.mention + " laughs at " + author.mention + " for even trying to execute the CE" + "*")
         elif author.id == 277453509018779648: #Hame
             if user.id == 116329127732051974:
                 await ctx.send("*" + user.mention + " dodges the bullet like a ninja" + "*")
             else:
                 await ctx.send("*" + author.mention + " executes " + user.mention + " with Nagant M1895 Revolver" + "*")
+        elif user.id == 116329127732051974: #KortProtect
+            await ctx.send("*" + user.mention + " laughs at " + author.mention + "*" + " You can't turn my own bot against me. Fool!")
         elif author.id == 116329127732051974: #Kort
             if user.id == 177403864369070080:
                 await ctx.send("*" + author.mention + " summons **MechaKorTech**!\n" + user.mention + " summons\n**MechaAschDroid**!\nThey start to battle and destroy everything around them!" + "*")
@@ -31,14 +33,12 @@ class ExecutionCog(commands.Cog):
             await ctx.send("*" + author.mention + " needs to let Kort know what he wants as a command" + "*")
         elif author.id == 185944398217871360: #HN
             await ctx.send("*" + author.mention + " purges the heresy of " + user.mention + " with cleansing flame." + "*")
-        elif author.id == 679023417315426344: #Sul
-            await ctx.send("*" + author.mention + " stalks silently in shadows. " + user.mention + " has failed for the last time..." + "*")
+        elif author.id == 679023417315426344: #WH
+            await ctx.send("*" + author.mention + " quickly unholsters his DL-44 and shoots first, killing " + user.mention + " instantly" + "*")
         elif author.id == 143549673947398145: #Control
             await ctx.send("*" + author.mention + " crushes " + user.mention + " beneath the treads of his tank for failing the Mother Islands" + "*")
         elif author.id == 505759332856496128: #FE
             await ctx.send("*" + author.mention + " calls on the mighty eagle battalion to dive from the sky delivering poop, crushing " + user.mention + " to death" + "*")
-        elif author.id == 356510817094598658: #ENE
-            await ctx.send("*" + author.mention + " assembles an army of snapping turtles, slowly enclosing " + user.mention + "  until they are reduced to a pile of bones" + "*")
         elif author.id == 418631163310112768: #Astro
             await ctx.send("*" + user.mention + " has been strapped to " + author.mention + "'s rocketship! They are blasting off into space, never to be seen again!" + "*")
         elif author.id == 373240532509392906: #Onfande
